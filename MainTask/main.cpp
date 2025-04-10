6bfce03b7a03c58346a79f0c1e8c69376791be70
ivanvikvik <ivanvikvik@gmail.com>
2025-04-10 18:54:33 +0300

    Add constructors with arguments in Student class добавление коснтрукторов с параметрами в класс Student

--- MainTask/main.cpp
+++ MainTask/main.cpp
@@ -2,20 +2,17 @@
 
 int main() {
 	//int array[10]{ 1,2,3 };
+		
+	Student student1;
+	Student student2("Alex");
+	Student student3("Matvey", 14);
+	Student student4("Bogdan", 15, 10, true);
 
-	Student student1{};
 
-	cout << "Before:\n";
 	cout << student1.toString() << endl;
-
-	student1.name = "Alex";
-	student1.age = 14;
-	student1.mark = 8.9;
-	student1.alive = true;
-
-	cout << "After:\n";
-	cout << student1.toString() << endl;
-
+	cout << student2.toString() << endl;
+	cout << student3.toString() << endl;
+	cout << student4.toString() << endl;
 
 	return 0;
 }
