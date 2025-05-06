b77750346a7235f5e17e4d6269d3d51a48731322
ivanvikvik <ivanvikvik@gmail.com>
2025-05-06 19:00:43 +0300

    Separate Initializer class to interface and implementation

--- MainTask/Initializer.h
+++ MainTask/Initializer.h
@@ -3,32 +3,5 @@
 // Function util class
 class Initializer {
 public:
-	void init(Student*& list, int count) {
-		if (list == nullptr && count > 0) {
-			list = new Student[count];
-		}
-
-		string names[]{ "Alex", "Matvey", "Veronika",
-			"Sofia", "Alice", "Anna", "Stepan",
-		"Daniil", "Arsenyi", "Vlad", "Bogdan", "Victor",
-		"Ilya", "Peter", "Harry", "Gleb" };
-
-		int minAge = 13;
-		int maxAge = 18;
-
-		int minMark = 0;
-		int maxMark = 10;
-
-		for (int i = 0; i < count; i++)
-		{
-			list[i].setName(names[rand() % 15]);
-			list[i].setAge(rand() % (maxAge - minAge + 1) + minAge);
-			for (int j = 0; j < list[i].getCountMarks(); j++)
-			{
-				//list[i].getMarks()[j] = rand() % (maxMark - minMark + 1) + minMark;
-				//*(list[i].getMarks() + j) = rand() % (maxMark - minMark + 1) + minMark;				
-				list[i].setMark(j, rand() % (maxMark - minMark + 1) + minMark);
-			}
-		}
-	}
+	void init(Student*& list, int count);	
 };
