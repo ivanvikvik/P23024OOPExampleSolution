0bbdb6db4e927f3de2f1893653af0b6b884d8205
ivanvikvik <ivanvikvik@gmail.com>
2025-05-15 18:46:07 +0300

    Add ArrayList class добавление динамического массива в виде структуры ArrayList-а

---
+++ ADSProject/ArrayList.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class ArrayList
+{
+private:
+	int* list;
+	int size;
+
+public:
+	ArrayList();
+	~ArrayList();
+
+	void add(int element);
+	void add(int index, int element);
+	void addAll(int* elements, int size);
+	//void addAll(int index, int* elements, int size);
+	void revome();
+	void remove(int index);
+	void clear();
+	bool isEmpty();
+	int get(int index);
+	void set(int index, int element);
+	int getSize();
+	string toString();
+};
+
