11a68f0ed23f7ad481b7be9312b2f010cdad8965
ivanvikvik <ivanvikvik@gmail.com>
2025-05-20 18:41:33 +0300

    Add Stack class

---
+++ ADSProject/Stack.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+// LIFO
+class Stack
+{
+private:
+	int* stack;
+	int size;
+
+public:
+	Stack();
+	~Stack();
+
+	void push(int element);
+	int pop();
+	int peek();
+	bool isEmpty();
+	int getSize();
+	void clear();
+	string toString();
+};
+
