fd8d9d2d8f4d59279af2b8dd33e06b597b56884d
ivanvikvik <ivanvikvik@gmail.com>
2025-05-29 19:50:04 +0300

    Add Point2D class

---
+++ PointProject/Point2D.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class Point2D
+{
+private:
+	int x;
+	int y;
+
+	double length();
+
+public:
+	// > < >= <= == !=
+	Point2D(): x(0), y(0) {}
+	Point2D(int x, int y) : x(x), y(y) {}
+	int getX() { return x; }
+	void setX(int x) { this->x = x; }
+	int getY() { return y; }
+	void setY(int y) { this->y = y; }
+
+	string toString();
+
+	bool operator==(Point2D point);
+	bool operator!=(Point2D point);
+	bool operator>(Point2D point);
+	bool operator<(Point2D point);
+	bool operator>=(Point2D point);
+	bool operator<=(Point2D point);
+
+	Point2D operator+(int value);
+	Point2D operator-(int value);
+	Point2D operator*(int value);
+	Point2D operator/(int value);
+
+	Point2D operator-();
+	Point2D operator++();		// ++point
+	Point2D operator++(int);	// point++
+
+
+};
+
